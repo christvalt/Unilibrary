5cd9330093b0de3d22d2ecd94f8fa8938e3ec18e
christ valtes medjouwo diagong <christ.medjoumo@studio.unibo.it>
2020-07-28 12:37:44 +0200

    create book

--- src/pages/adminf/createbook.js
+++ src/pages/adminf/createbook.js
@@ -9,7 +9,38 @@ function Createbook() {
         <a href="#Book">Book</a> {""} /{""} <a href="#Book">New Book</a>
       </h3>
 
-      <form></form>
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form.Item
+          name="note"
+          label="Note"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="gender"
+          label="Gender"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Select
+            placeholder="Select a option and change input text above"
+            onChange={onGenderChange}
+            allowClear
+          >
+            <Option value="male">male</Option>
+            <Option value="female">female</Option>
+            <Option value="other">other</Option>
+          </Select>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
